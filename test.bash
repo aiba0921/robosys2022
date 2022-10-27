3dd69dee67a470a62733cea8e65e85904b1d68fe
aiba0921 <s21C1004GV@s.chibakoudai.jp>
2022-10-27 12:27:40 +0900

    update test.bash

--- test.bash
+++ test.bash
@@ -1,3 +1,19 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2022 Yuto Aiba
+#SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK   # && (AND記号)は左側が成功すると右側を実行
+exit $res
+
